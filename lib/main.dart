6ccb43638f1f9daef4480d7c1284975955f50a4c
saobang001111@gmail.com <you@example.com>
2021-11-02 22:14:45 +0700

    Update: Provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/providers/products.dart';
+import 'package:shop_app/screens/product_detail_screen.dart';
 import 'package:shop_app/screens/products_overview_screen.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(const MyApp());
-
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MyShop',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: ProductsOverviewScreen(),
+    return ChangeNotifierProvider(
+      create: (BuildContext context) => Products(),
+      child: MaterialApp(
+          title: 'MyShop',
+          theme: ThemeData(
+            primarySwatch: Colors.purple,
+            accentColor: Colors.deepOrange,
+            fontFamily: 'Lato',
+          ),
+          home: ProductsOverviewScreen(),
+          routes: {
+            ProductDetailScreen.routeName: (ctx) => const ProductDetailScreen(),
+          }),
     );
   }
 }
@@ -31,4 +40,3 @@ class MyHomePage extends StatelessWidget {
     );
   }
 }
-
--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,3 +1,5 @@
+
+
 class Product {
   final String id;
   final String title;
